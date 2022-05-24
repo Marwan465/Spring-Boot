536a0eeeceab7fa8e9dab4d38a34b0dc180b1b16
Marwan465 <marwanshade@gmail.com>
2022-05-24 18:52:16 +0200

    initial commit

---
+++ src/main/java/com/example/DogMicroservice/entity/Dog.java
@@ -0,0 +1,91 @@
+package com.example.DogMicroservice.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Component
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String breed;
+    private String origin;
+    Dog(){}
+    Dog(long id, String name, String breed, String origin) {
+        this.id=id;
+        this.name=name;
+        this.breed=breed;
+        this.origin=origin;
+    }
+    Dog( String name, String breed, String origin) {
+        this.name=name;
+        this.breed=breed;
+        this.origin=origin;
+    }
+
+    
+
+    /**
+     * @return long return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the breed
+     */
+    public String getBreed() {
+        return breed;
+    }
+
+    /**
+     * @param breed the breed to set
+     */
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    /**
+     * @return String return the origin
+     */
+    public String getOrigin() {
+        return origin;
+    }
+
+    /**
+     * @param origin the origin to set
+     */
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+}
+
---
+++ src/main/java/com/example/DogMicroservice/repository/dogRepository.java
@@ -0,0 +1,22 @@
+package com.example.DogMicroservice.repository;
+
+import java.util.List;
+
+import com.example.DogMicroservice.entity.Dog;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface dogRepository extends CrudRepository<Dog, Long> {
+    
+    @Query("select d.id, d.breed from Dog d where d.id=:id")
+    String findBreedById(Long id);
+
+    @Query("select d.id, d.breed from Dog d")
+    List<String> findAllBreed();
+
+    @Query("select d.id, d.name from Dog d")
+    List<String> findAllName();
+}
+
+
